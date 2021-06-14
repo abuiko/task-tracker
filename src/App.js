14a07ce11e31efabf034cab7c018df5bc81058c7
Alena Buiko <abuiko.ma@gmail.com>
2021-06-14 17:56:02 -0400

    fill out form / add new task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
 
 function App() {
   const [tasks, setTasks] = useState(
@@ -27,6 +28,17 @@ function App() {
     ]
   )
 
+
+
+  // add task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   // Delete task
 
   const deleteTask = (id) => {
@@ -42,6 +54,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
